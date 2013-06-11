ff1462d66b82ef933aad9413ec1159e03012c5df
Alex Bakula-Davis <abakuladavis@gmail.com>
2013-06-10 17:09:46 -0700

    Initial Vegas Blog Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '37dee611e576e900af4369c7d3b05e9e4c26f15a9d03bffae9e0b809c915628b4c5b05e6337052731ac1ab59aa096e0bbb52fd05f66bad585e10866b7c2b4873'
