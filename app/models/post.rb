ccf1a6f0e7425ddc04b45079b746f76619366ce6
Alex Bakula-Davis <abakuladavis@gmail.com>
2013-06-17 16:01:28 -0700

    Edited post / comment language

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :tags_attributes
+  attr_accessible :content, :title
 
   
   validates :title, :presence => true,
@@ -7,8 +7,7 @@ class Post < ActiveRecord::Base
   validates :content, :length => {:minimum => 5}
 
   has_many :comments, :dependent => :destroy
-  has_many :tags
+  
  
-  accepts_nested_attributes_for :tags, :allow_destroy => :true,
-    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  
 end
