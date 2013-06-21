7963264c0e7a570c2581a5b83d8fe3464cd65046
Alex Bakula-Davis <abakuladavis@gmail.com>
2013-06-21 11:02:10 -0700

    Linked comments to index posts

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'Posts#index'
 
   # See how all your routes lay out with "rake routes"
 
